51e7180d4eb87efa0ecad8a87ea6be7b49eda953
ehsan nosair <ehsannosair@gmail.com>
2025-01-05 08:52:39 +0300

    Fix tests: Add necessary providers to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace EhsanNosair\Chativel\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Filament\FilamentServiceProvider;
+use Livewire\LivewireServiceProvider;
+use Filament\Forms\FormsServiceProvider;
+use Filament\Tables\TablesServiceProvider;
+use BladeUI\Icons\BladeIconsServiceProvider;
+use Filament\Actions\ActionsServiceProvider;
+use Filament\Support\SupportServiceProvider;
+use Filament\Widgets\WidgetsServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Filament\Infolists\InfolistsServiceProvider;
 use EhsanNosair\Chativel\ChativelServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
+use Filament\Notifications\NotificationsServiceProvider;
+use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -20,6 +32,18 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
+            ActionsServiceProvider::class,
+            BladeCaptureDirectiveServiceProvider::class,
+            BladeHeroiconsServiceProvider::class,
+            BladeIconsServiceProvider::class,
+            FilamentServiceProvider::class,
+            FormsServiceProvider::class,
+            InfolistsServiceProvider::class,
+            LivewireServiceProvider::class,
+            NotificationsServiceProvider::class,
+            SupportServiceProvider::class,
+            TablesServiceProvider::class,
+            WidgetsServiceProvider::class,
             ChativelServiceProvider::class,
         ];
     }
